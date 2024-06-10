25adaf881e69ee82b22aafd83ab639692807ae98
Michalis111 <michalis.iacovides2002@gmail.com>
2024-06-11 00:42:20 +0300

    implement clerk webhooks

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -60,7 +60,9 @@ export async function POST(req: Request) {
   // CREATE
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
-
+  // Ensure first_name and last_name are not null
+    const firstName = first_name || "";
+    const lastName = last_name || "";
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
