0e541db4bf7b3e5c4798296fe573ec70a21552d2
Michalis111 <michalis.iacovides2002@gmail.com>
2024-06-10 17:15:20 +0300

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,12 +8,12 @@ import { ClerkProvider } from "@clerk/nextjs";
 const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"],
   weight: ['400', '500', '600', '700'],
+  variable: '--font-ibm-plex'
  });
 
 export const metadata: Metadata = {
   title: "Audify",
   description: "AI-powered audio generator",
-  variable: '--font-ibm-plex'
 };
 
 export default function RootLayout({
